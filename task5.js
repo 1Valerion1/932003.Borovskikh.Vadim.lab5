519f0c50209d4809a195f16bc9bf81e242135e1d
Vadim <59086091+1Valerion1@users.noreply.github.com>
2022-10-16 15:04:40 +0700

    One

---
+++ task5.js
@@ -0,0 +1,24 @@
+open1.onclick= function(){
+    backk.style.display = "block";
+    ow1.style.display = "block";
+}
+open2.onclick= function(){
+    backk.style.display = "block";
+    ow3.style.display = "block";
+}
+open3.onclick= function(){
+    backk.style.display = "block";
+    ow2.style.display = "block";
+}
+ow2.onclick= function(){
+    backk.style.display = "none";
+    ow2.style.display = "none";
+}
+ow3.onclick= function(){
+    backk.style.display = "none";
+    ow3.style.display = "none";
+}
+ow1.onclick= function(){
+    backk.style.display = "none";
+    ow1.style.display = "none";
+}
